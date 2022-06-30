42b20abc338579ee425c842faddfcf28bf7395ce
Tom Dimiduk <tom@dimiduk.net>
2022-06-29 19:54:40 -0700

    inital import

---
+++ custom.el
@@ -0,0 +1,17 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((git-commit-major-mode . git-commit-elisp-text-mode)
+     (dante-repl-command-line
+      ("nix-shell" "--run" "cabal repl --builddir=dist/dante lib:ficsift"))
+     (haskell-process-use-ghci . t)
+     (haskell-indent-spaces . 4))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
